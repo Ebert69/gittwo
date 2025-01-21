92ef4348424c10cc19210e1288e3329d95776080
Ebert69 <halbendorf23@aol.de>
2025-01-21 13:04:09 +0100

    Stern-Ansatz

---
+++ stars2.dart
@@ -0,0 +1,26 @@
+class Hexagon{
+  int radius = 0;
+  var ursprung_x = 0;
+  var ursprung_y = 0;
+  var vertex_map ={  //soll mit radius multipliziert werden, mitursprung addiert
+    'v_1_x' : 1, //Punkt oben, mittig, Uhrzeigersinn
+    'v_1_y' : -1,
+    'v_2_x' : 1.819,
+    'v_2_y' : -0.5,
+    'v_3_x' : 1.819,
+    'v_3_y' : 0.5,
+    'v_4_x' : 1,
+    'v_4_y' : 2,
+  };
+  var koordinaten_liste = [];
+
+
+}
+
+void main(){
+  var hexagon = Hexagon();
+  hexagon.radius = 10;
+  hexagon.ursprung_x = 0;
+  hexagon.ursprung_y = 0;
+
+}
